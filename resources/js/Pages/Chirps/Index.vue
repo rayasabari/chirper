d7e5494b00b1108f266aecca13b7cb80f80396d2
Raya Sabari <hi@rayasabari.com>
2023-01-25 13:18:38 +0700

    Auth Sidebar Layouts

--- resources/js/Pages/Chirps/Index.vue
+++ resources/js/Pages/Chirps/Index.vue
@@ -8,24 +8,30 @@ import { useForm, Head } from '@inertiajs/inertia-vue3';
 defineProps(['chirps']);
 
 const form = useForm({
-    message: '',
+  message: '',
 });
 </script>
  
 <template>
-    <Head title="Chirps" />
 
-    <AuthenticatedLayout>
-        <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
-            <form @submit.prevent="form.post(route('chirps.store'), { onSuccess: () => form.reset() })">
-                <textarea v-model="form.message" placeholder="What's on your mind?"
-                    class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"></textarea>
-                <InputError :message="form.errors.message" class="mt-2" />
-                <PrimaryButton class="mt-4">Chirp</PrimaryButton>
-            </form>
-            <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
-                <Chirp v-for="chirp in chirps" :key="chirp.id" :chirp="chirp" />
-            </div>
-        </div>
-    </AuthenticatedLayout>
+  <Head title="Chirps" />
+
+  <AuthenticatedLayout>
+    <template #header>
+      <h2 class="text-xl font-semibold leading-tight text-gray-800">
+        {{ 'Chirps' }}
+      </h2>
+    </template>
+    <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+      <form @submit.prevent="form.post(route('chirps.store'), { onSuccess: () => form.reset() })">
+        <textarea v-model="form.message" placeholder="What's on your mind?"
+          class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"></textarea>
+        <InputError :message="form.errors.message" class="mt-2" />
+        <PrimaryButton class="mt-4">Chirp</PrimaryButton>
+      </form>
+      <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
+        <Chirp v-for="chirp in chirps" :key="chirp.id" :chirp="chirp" />
+      </div>
+    </div>
+  </AuthenticatedLayout>
 </template>
